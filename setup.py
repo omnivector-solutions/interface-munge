a850d5d433eb79ce0ae59f1cedd0374069337b31
jesse <jesse1517@gmail.com>
2020-06-22 23:07:50 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='interface-munge',
+    packages=find_packages(include=['interface_munge']),
+    version='0.0.1',
+    license='MIT',
+    long_description=open('README.md').read(),
+    install_requires=[],
+)
