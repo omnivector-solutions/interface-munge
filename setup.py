83d18ac79c9ff0a3b9316c26703b134c2e4234c3
jesse <jesse1517@gmail.com>
2020-06-24 02:37:57 +0000

    keyring error

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setuptools, find_packages
+
+with  open('README.md') as fh:
+    long_description = fh.read()
 
 
-setup(
-    name='interface-munge',
+setuptools.setup(
+    name='interface-munge-jesseleo',
     packages=find_packages(include=['interface_munge']),
     version='0.0.1',
     license='MIT',
-    long_description=open('README.md').read(),
-    install_requires=[],
+    long_description= long_description,
+    url='https://github.com/omnivector-solutions/interface-munge',
+    python_requires='>=3.6',
 )
